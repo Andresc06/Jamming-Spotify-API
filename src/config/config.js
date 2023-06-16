6f86a716c5762294cefbb02b2632e08df2fc71c5
Andresc06 <andrescontreras370@gmail.com>
2023-06-15 20:39:57 -0500

    chore: remove .env file to deploy

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,5 @@
 const config = {
   CLIENT_KEY: import.meta.env.VITE_CLIENT_KEY,
-  CLIENT_SECRET_KEY: import.meta.env.VITE_CLIENT_SECRET_KEY,
   USER_ID: import.meta.env.VITE_USER_ID,
 }
 
--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  envDir: './buildConfig/environments',
+  VITE_CLIENT_KEY: process.env.VITE_CLIENT_KEY,
+  VITE_USER_ID: process.env.VITE_USER_ID,
 })
